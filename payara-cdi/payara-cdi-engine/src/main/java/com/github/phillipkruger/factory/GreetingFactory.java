9b054fc85ab47776dcf90f9d3cd20dddacd21c1e
Phillip Kruger <phillip.kruger@gmail.com>
2017-12-10 16:57:00 +0200

    Payara CDI working

--- payara-cdi/payara-cdi-engine/src/main/java/com/github/phillipkruger/factory/GreetingFactory.java
+++ payara-cdi/payara-cdi-engine/src/main/java/com/github/phillipkruger/factory/GreetingFactory.java
@@ -3,20 +3,21 @@ package com.github.phillipkruger.factory;
 import com.github.phillipkruger.factory.api.Greeting;
 import com.github.phillipkruger.factory.api.GreetingProviderLiteral;
 import com.github.phillipkruger.factory.impl.English;
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
-@ApplicationScoped
+@RequestScoped
 public class GreetingFactory {
     
     @Inject @Any
     private Instance<Greeting> greetings;
  
     public Greeting getGreeting(String name) {
-        Greeting provider = greetings.select(new GreetingProviderLiteral(name)).get();
-        if(provider!=null){
+        Instance<Greeting> instance = greetings.select(new GreetingProviderLiteral(name));
+        if(!instance.isUnsatisfied()){
+            Greeting provider = instance.get();
             return provider;
         }else{
             return new English();
